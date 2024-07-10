8e27f0863de0c458f8ccdf7b244cdb1abe928d53
Thomas Winckelman <61361842+ThomasLastName@users.noreply.github.com>
2024-07-10 13:09:48 -0600

    Update repo url

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name = 'test',
     version = '1.0.0',
-    url = 'https://github.com/ThomasLastName/test.git',
+    url = 'https://github.com/ThomasLastName/slug.git',
     author = 'Thomas Winckelman',
     author_email = 'winckelman@tamu.edu',
     description = 'Description of my package',
