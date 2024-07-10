0caebd628c914cebca090466c97e8c53ffee50b6
Thomas Winckelman <61361842+ThomasLastName@users.noreply.github.com>
2024-07-10 13:08:05 -0600

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup, find_packages
+
+setup(
+    name = 'test',
+    version = '1.0.0',
+    url = 'https://github.com/ThomasLastName/test.git',
+    author = 'Thomas Winckelman',
+    author_email = 'winckelman@tamu.edu',
+    description = 'Description of my package',
+    packages = find_packages(),    
+    install_requires = [],
+)
