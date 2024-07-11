91ce3df8e3ff941d9cfdf0ae61e839596f4d8228
Thomas Winckelman <61361842+ThomasLastName@users.noreply.github.com>
2024-07-10 18:18:03 -0600

    Aesthetic change to comments

--- package_name/__init__.py
+++ package_name/__init__.py
@@ -1,4 +1,7 @@
-__version__ = "1.1.0"
 
+#
+# ~~~ Automatically correspond with the version from setup.py (chat-gpt showed me this one)
 from pkg_resources import get_distribution, DistributionNotFound
 __version__ = get_distribution('package_name').version
+
+
