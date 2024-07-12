9b24190b2301666ffd8197f82ccbb73debd2ad17
Thomas Winckelman <61361842+ThomasLastName@users.noreply.github.com>
2024-07-11 22:29:46 -0600

    Delete lightweight directory

--- lightweight/__init__.py
+++
@@ -1,4 +0,0 @@
-#
-# ~~~ Automatically correspond with the version from setup.py (chat-gpt showed me this one)
-from pkg_resources import get_distribution, DistributionNotFound
-__version__ = get_distribution('lightweight').version
