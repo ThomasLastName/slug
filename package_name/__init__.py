68cdfb64cf8c4182019bd713dc5916e77c87a276
Thomas Winckelman <61361842+ThomasLastName@users.noreply.github.com>
2024-07-12 14:17:19 -0600

    More minimal

--- package_name/__init__.py
+++ package_name/__init__.py
@@ -1,5 +1,1 @@
-#
-# ~~~ Automatically correspond with the version and url from setup.py (chat-gpt showed me this one)
-from pkg_resources import get_distribution, DistributionNotFound
-__version__ = get_distribution('package_name').version
 
