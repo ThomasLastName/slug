de4e0fcb112ab814f3481878bded77f941838626
Thomas Winckelman <61361842+ThomasLastName@users.noreply.github.com>
2025-01-13 18:40:34 -0600

    Automatically infer package name

--- setup.py
+++ setup.py
@@ -8,16 +8,16 @@ from setuptools import setup, find_packages
 #
 # ~~ Load the a .txt file, into a list of strings (each line is a string in the list)
 def txt_to_list(filepath):
-    with open(filepath, 'r') as f:
+    with open(filepath, "r") as f:
         return [line.strip() for line in f]
 
 setup(
-    name = 'package_name',
-    version = '1.3.0',
-    url = 'https://github.com/ThomasLastName/slug',
-    author = 'Thomas Winckelman',
-    author_email = 'winckelman@tamu.edu',
-    description = 'Description of my package',
+    name = find_packages()[0], # ~~~ assumes there's only package in the directory where `setup.py` is located; otherwise, enter it manually: e.g., "package_name"
+    version = "1.3.0",
+    url = "https://github.com/ThomasLastName/slug",
+    author = "Thomas Winckelman",
+    author_email = "winckelman@tamu.edu",
+    description = "Description of my package",
     packages = find_packages(),    
     install_requires = txt_to_list("requirements.txt") # ~~~ when you pip install `package_name`, pip will also install `pyreadr`
 )
